678412a23839811e12a1ea65e611483f46a68b72
Oriane <oriane.louis.pro@gmail.com>
2021-02-11 21:05:05 -0500

    feat: Draw the board of the snake

---
+++ src/components/Board.vue
@@ -0,0 +1,26 @@
+<template>
+  <canvas id="board" ref="board" :width="width" :height="height"></canvas>
+</template>
+
+<script>
+export default {
+  name: "Board",
+  props: {
+    width: Number,
+    height: Number,
+    color: String,
+  },
+  mounted() {
+    const canvas = this.$refs.board;
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = this.color;
+    ctx.fillRect(0, 0, this.width, this.height);
+  },
+};
+</script>
+
+<style>
+#board {
+  border: 1px solid black;
+}
+</style>
