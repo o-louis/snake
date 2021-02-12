aad1bf7d46aa21fd60677ca2795e2d90f1e7eadc
Oriane <oriane.louis.pro@gmail.com>
2021-02-11 23:03:08 -0500

    feat: Grow the snake when food is eaten

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,12 @@ export default {
       snake: {
         size: 20,
         color: "green",
-        pos: {
-          x: 0,
-          y: 0,
-        },
+        pos: [
+          {
+            x: 0,
+            y: 0,
+          },
+        ],
       },
       apple: {
         size: 20,
@@ -57,7 +59,7 @@ export default {
       this.board.heightRect = this.board.height / this.snake.size;
     },
     initSnake() {
-      this.snake.pos = this.getRandomPosition();
+      this.snake.pos[0] = this.getRandomPosition();
       this.drawSnake();
     },
     initFood() {
@@ -70,13 +72,10 @@ export default {
       this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     },
     drawSnake() {
-      this.ctx.fillStyle = this.snake.color;
-      this.ctx.fillRect(
-        this.snake.pos.x,
-        this.snake.pos.y,
-        this.snake.size,
-        this.snake.size
-      );
+      this.snake.pos.forEach((pos) => {
+        this.ctx.fillStyle = this.snake.color;
+        this.ctx.fillRect(pos.x, pos.y, this.snake.size, this.snake.size);
+      });
     },
     drawApple() {
       this.ctx.fillStyle = this.apple.color;
@@ -90,7 +89,7 @@ export default {
     handleDirection() {
       const _this = this;
       document.addEventListener("keydown", (e) => {
-        let { x, y } = _this.snake.pos;
+        let { x, y } = _this.snake.pos[0];
         switch (e.key) {
           case "ArrowLeft":
             x = x - this.snake.size >= 0 ? x - this.snake.size : 0;
@@ -108,21 +107,21 @@ export default {
             break;
           default:
         }
-        _this.snake.pos = { x, y };
+
         _this.clearBoard();
-        if (_this.isFoodEaten()) {
+        _this.snake.pos.unshift({ x, y });
+        if (_this.isFoodEaten({ x, y })) {
           this.initFood();
         } else {
           _this.drawApple();
+          _this.snake.pos[0] = { x, y };
+          _this.snake.pos.pop();
         }
         _this.drawSnake();
       });
     },
-    isFoodEaten() {
-      return (
-        this.snake.pos.x === this.apple.pos.x &&
-        this.snake.pos.y === this.apple.pos.y
-      );
+    isFoodEaten({ x, y }) {
+      return x === this.apple.pos.x && y === this.apple.pos.y;
     },
     getRandomPosition() {
       const x =
@@ -130,7 +129,7 @@ export default {
       const y =
         Math.floor(Math.random() * this.board.heightRect) * this.snake.size;
 
-      if (this.snake.pos === { x, y }) this.getRandomPosition();
+      if (this.snake.pos[0] === { x, y }) this.getRandomPosition();
       if (this.apple.pos === { x, y }) this.getRandomPosition();
 
       return { x, y };
